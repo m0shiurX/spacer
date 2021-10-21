f2437891f7fab139f00c6e9e80365f16e9849dcf
Moshiur Rahman <unimrgm@gmail.com>
2021-10-22 01:33:58 +0600

    poppins font added as template

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,28 @@ module.exports = {
   purge: ['./*.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          'DEFAULT': '1rem',
+          'sm': '2rem',
+          'lg': '3rem',
+          'xl': '4rem',
+        }
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      zIndex: {
+        '-1': '-1',
+       }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    // require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'),
   ],
 }
