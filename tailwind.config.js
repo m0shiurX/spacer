f5deec5986268fdf36151fed0b413c0b79044de9
Moshiur Rahman <unimrgm@gmail.com>
2021-10-21 12:03:57 +0600

    Initialize tailwind-alpine-vite starter app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
+}
